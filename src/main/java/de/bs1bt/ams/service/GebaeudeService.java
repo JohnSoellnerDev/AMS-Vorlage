fa2aee126046288bde9a4154e45050778a8063b9
JohnSoellnerDev <tridlegames@gmail.com>
2026-02-03 14:24:21 +0100

    Add GebaeudeService for building management with CRUD operations

---
+++ src/main/java/de/bs1bt/ams/service/GebaeudeService.java
@@ -0,0 +1,44 @@
+package de.bs1bt.ams.service;
+
+import de.bs1bt.ams.model.Gebaeude;
+import de.bs1bt.ams.repository.GebaeudeJpaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class GebaeudeService {
+
+    private final GebaeudeJpaRepository gebaeudeRepository;
+
+    @Autowired
+    public GebaeudeService(GebaeudeJpaRepository gebaeudeRepository) {
+        this.gebaeudeRepository = gebaeudeRepository;
+    }
+
+    public List<Gebaeude> getAllGebaeude() {
+        return gebaeudeRepository.findAll();
+    }
+
+    public Optional<Gebaeude> getGebaeudeById(Integer id) {
+        return gebaeudeRepository.findById(id);
+    }
+
+    public Gebaeude createGebaeude(Gebaeude gebaeude) {
+        return gebaeudeRepository.save(gebaeude);
+    }
+
+    public Optional<Gebaeude> updateGebaeude(int id, Gebaeude gebaeudeDetails) {
+        return gebaeudeRepository.findById(id).map(gebaeude -> {
+            gebaeude.setBezeichnung(gebaeudeDetails.getBezeichnung());
+            gebaeude.setAdresse(gebaeudeDetails.getAdresse());
+            return gebaeudeRepository.save(gebaeude);
+        });
+    }
+
+    public void deleteGebaeude(int id) {
+        gebaeudeRepository.deleteById(id);
+    }
+}
