cbad8c2dff93f3c43fd6390e7b5a8fb17e10772d
JohnSoellnerDev <tridlegames@gmail.com>
2026-02-05 10:12:05 +0100

    Improved Organisation

--- mvc-observer-tasks/src/main/java/de/bs1bt/task1/Subject.java
+++
@@ -1,7 +0,0 @@
-package de.bs1bt.task1;
-
-public interface Subject {
-    void addObserver(Observer o);
-    void removeObserver(Observer o);
-    void notifyObservers();
-}
--- src/main/java/de/bs1bt/ams/AMSSpringBootApplication.java
+++ src/main/java/de/bs1bt/ams/AMSSpringBootApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AMSSpringBootApplication {
 
-    public static void main(String[] args) {
+    void main(String[] args) {
         SpringApplication.run(AMSSpringBootApplication.class, args);
     }
 }
